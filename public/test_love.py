1b6b3083ee50823466f7b70a83ecfcf893707d4b
MD367075bd <mdanarul7075@gmail.com>
2021-11-12 00:51:37 +0600

    version 1.0

---
+++ public/test_love.py
@@ -0,0 +1,20 @@
+name = "Hello World"
+
+nameInput = input('Enter the your Favorite name: ')
+
+def name(n):
+	name = f"I Love You {nameInput}"
+	if "Anarul" == nameInput:
+		return name
+	elif "Angur" == nameInput:
+		return name 
+	elif 'Amena' == nameInput:
+		return name
+	else:
+		print('Your Favorite name is not Found... and This name is very bad for you.')
+	return n
+
+FavoriteNameTest = name(nameInput)
+
+
+
